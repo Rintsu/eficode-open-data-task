76499a5f51a2551b67ec051a28e423a459b3d4b1
Kuukso Riina <s1800242@student.businesscollege.fi>
2019-03-06 02:33:13 +0200

    Updates

---
+++ precache-manifest.d353d3c9e219c5db3980ddcb807408f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17fe91f221dba99a3c5c",
+    "url": "/eficode-open-data-task/static/js/runtime~main.17fe91f2.js"
+  },
+  {
+    "revision": "a233d43ec899c1c3fcf1",
+    "url": "/eficode-open-data-task/static/js/main.a233d43e.chunk.js"
+  },
+  {
+    "revision": "cac5eee00757c79e6694",
+    "url": "/eficode-open-data-task/static/js/2.cac5eee0.chunk.js"
+  },
+  {
+    "revision": "a233d43ec899c1c3fcf1",
+    "url": "/eficode-open-data-task/static/css/main.ca847f0b.chunk.css"
+  },
+  {
+    "revision": "d86594f063bed9866d9afdee3d9ff5cf",
+    "url": "/eficode-open-data-task/index.html"
+  }
+];
